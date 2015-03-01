cec8cb9baaa4af61a1936b866a50ccba175a560f
sanemat <o.gata.ken@gmail.com>
2015-03-01 17:33:06 +0900

    chore(travis): check file
    see:
    Jenkinsのシェルスクリプトで0以外の終了コードを判定する方法 - Qiita
    http://qiita.com/mechamogera/items/4f28d1f71838b75cc979

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -3,6 +3,20 @@ set -e
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install saddler saddler-reporter-github
 
+  echo "gif diff"
+  git diff --name-only origin/master \
+     | grep -a '\.md$' || RETURN_CODE=$?
+
+  case "$RETURN_CODE" in
+    "" ) echo "found" ;;
+    "1" )
+      echo "not found"
+      exit 0 ;;
+    * )
+      echo "Error"
+      exit $RETURN_CODE ;;
+  esac
+
   git diff --name-only origin/master \
    | grep -a '\.md$' \
    | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
