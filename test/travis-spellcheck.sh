8e20941eb0f37b8554bb07a2e8dcd711e2470853
azu <azuciao@gmail.com>
2015-03-01 13:35:51 +0900

    fix(test): fix spellcheck script

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -4,7 +4,7 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
   gem install saddler saddler-reporter-github
 
   git diff -z --name-only origin/master \
-   | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle
+   | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
    | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestComment
