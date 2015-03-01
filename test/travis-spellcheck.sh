f83d34a099ff6e67020b8fbd383e758ddd681aa6
azu <azuciao@gmail.com>
2015-03-01 19:23:54 +0900

    chore(travis): update script

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -3,9 +3,14 @@ set -e
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install saddler saddler-reporter-github
 
-  echo "gif diff"
+  echo "= gif diff"
   git diff --name-only origin/master \
    | grep -e '\.md$'
+
+  echo "= checkstyle"
+  git diff -z --name-only origin/master \
+     | grep -e '\.md$' \
+     | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle
 
   git diff -z --name-only origin/master \
    | grep -e '\.md$' \
