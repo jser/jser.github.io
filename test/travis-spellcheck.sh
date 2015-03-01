8c943e3c7c95703307d1348d80f60578e7c4d5e7
azu <azuciao@gmail.com>
2015-03-01 13:51:12 +0900

    chore(test): add filter by markdown

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -3,7 +3,11 @@ set -v
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install saddler saddler-reporter-github
 
+  git diff --name-only origin/master \
+   | grep -e '\.md$'
+
   git diff -z --name-only origin/master \
+   | grep -e '\.md$'
    | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
    | saddler report \
       --require saddler/reporter/github \
