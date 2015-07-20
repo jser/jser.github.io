6d3c7fa24c79b87b7a038f4dc596af3259e9959c
azu <azuciao@gmail.com>
2015-07-20 14:08:15 +0900

    refactor: extra checker and parser

--- test/category-checker.js
+++ test/category-checker.js
@@ -1,37 +1,17 @@
 // LICENSE : MIT
 "use strict";
-var fs = require("fs");
 var glob = require("glob");
 var path = require("path");
-var fm = require('front-matter');
 var targetDir = path.join(__dirname, "..", "_i18n");
 var files = glob.sync(targetDir + "/**/*.md");
-
-function Checker(file) {
-    this.file = file;
-    this.parsedTree = fm(fs.readFileSync(file, "utf-8"));
-}
-Checker.prototype.getCategories = function () {
-    var cats = [];
-    if (this.parsedTree.attributes["categories"]) {
-        cats.concat(this.parsedTree.attributes["categories"].fileter(function (name) {
-            return name.length > 0;
-        }));
-    }
-    if (this.parsedTree.attributes["category"] && this.parsedTree.attributes["category"].length > 0) {
-        cats.push(this.parsedTree.attributes["category"]);
-    }
-    return cats;
-};
-
+var MDFileParser = require("./lib/MDFileParser");
 function isEmptyCategory(categories) {
     return categories.length === 0;
 }
 
-
 // <MAIN>
 var warningFiles = files.map(function (filePath) {
-    return new Checker(filePath);
+    return new MDFileParser(filePath);
 }).filter(function (checker) {
     return isEmptyCategory(checker.getCategories());
 }).map(function (checker) {
---
+++ test/lib/MDFileParser.js
@@ -0,0 +1,22 @@
+// LICENSE : MIT
+"use strict";
+var fs = require("fs");
+var fm = require('front-matter');
+function MDFileParser(file) {
+    this.file = file;
+    this.parsedTree = fm(fs.readFileSync(file, "utf-8"));
+}
+MDFileParser.prototype.getCategories = function () {
+    var cats = [];
+    if (this.parsedTree.attributes["categories"]) {
+        cats.concat(this.parsedTree.attributes["categories"].fileter(function (name) {
+            return name.length > 0;
+        }));
+    }
+    if (this.parsedTree.attributes["category"] && this.parsedTree.attributes["category"].length > 0) {
+        cats.push(this.parsedTree.attributes["category"]);
+    }
+    return cats;
+};
+
+module.exports = MDFileParser;
