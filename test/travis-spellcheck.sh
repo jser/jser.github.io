959234ce949a30d0e8c12e73105722bf12cdb242
azu <azuciao@gmail.com>
2015-09-25 21:42:49 +0900

    fix(script): fix spell check script

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
+
+  diffBranchName="origin/develop"
   # 変更行のみを対象にする
-  npm run textlint -- -f checkstyle \
-   | checkstyle_filter-git diff develop \
-   | saddler report \
+  git diff --name-only --diff-filter=ACMR ${diffBranchName} \
+  | grep -a '_i18n/ja/.*.md$' \
+  | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
+  | checkstyle_filter-git diff ${diffBranchName} \
+  | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
 fi
