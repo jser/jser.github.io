882c94204ba29fa7632d8902be30bdf169a89696
azu <azuciao@gmail.com>
2015-09-25 21:24:09 +0900

    direct textlint

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -4,7 +4,7 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
 
   echo "before: npm run textlint checkstyle"
   npm run textlint --silent
-  npm run textlint -- -f checkstyle | checkstyle_filter-git diff origin/develop
+  git diff --name-only --diff-filter=ACMR origin/develop | grep -a '_i18n/ja/.*.md$' | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle | checkstyle_filter-git diff origin/develop
   echo "after: npm run textlint checkstyle"
   # 変更行のみを対象にする
   npm run textlint --silent -- -f checkstyle \
