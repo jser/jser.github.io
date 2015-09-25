18b5125001d0165da53938a7964c277318be6e5f
azu <azuciao@gmail.com>
2015-09-25 20:47:43 +0900

    fix(script): remove set -e

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-set -e
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
-
-  npm run textlint && exit 0
-
   # 変更行のみを対象にする
   npm run textlint -- -f checkstyle \
    | checkstyle_filter-git diff develop \
