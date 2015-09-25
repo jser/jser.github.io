b034c6f6600b9c100ea558edc13f3e812860b6e1
azu <azuciao@gmail.com>
2015-09-25 20:55:40 +0900

    fix(script): add diff target

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -3,7 +3,7 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
   # 変更行のみを対象にする
   npm run textlint --silent -- -f checkstyle \
-   | checkstyle_filter-git diff develop \
+   | checkstyle_filter-git diff origin/develop \
    | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
