03e260d75d3c62f006cec18ca153d07f152cf403
azu <azuciao@gmail.com>
2015-09-25 21:28:01 +0900

    add \

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -6,7 +6,7 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
   git diff --name-only --diff-filter=ACMR origin/develop \
   | grep -a '_i18n/ja/.*.md$' \
   | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
-  | checkstyle_filter-git diff origin/develop
+  | checkstyle_filter-git diff origin/develop \
   | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
