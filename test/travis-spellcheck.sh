f6d632c1f6ab31c7d6604fe3022562f04f54641b
azu <azuciao@gmail.com>
2015-09-25 21:37:35 +0900

    use variable

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -2,11 +2,12 @@
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
+  diffBranchName="origin/develop"
   # 変更行のみを対象にする
-  git diff --name-only --diff-filter=ACMR origin/develop \
+  git diff --name-only --diff-filter=ACMR ${diffBranchName} \
   | grep -a '_i18n/ja/.*.md$' \
   | xargs $(npm bin)/textlint --rulesdir test/rules -f checkstyle \
-  | checkstyle_filter-git diff origin/develop \
+  | checkstyle_filter-git diff ${diffBranchName} \
   | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
