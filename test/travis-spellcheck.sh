8194d1df8b96f2ba005b3d9400274a06caa77448
azu <azuciao@gmail.com>
2015-09-25 21:05:55 +0900

    add debug code

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
+
+  npm run textlint --silent -- -f checkstyle
   # 変更行のみを対象にする
   npm run textlint --silent -- -f checkstyle \
-   | checkstyle_filter-git diff origin/develop \
-   | saddler report \
+  | checkstyle_filter-git diff origin/develop \
+  | saddler report \
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
 fi
