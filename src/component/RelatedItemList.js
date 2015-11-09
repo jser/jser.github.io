b2d21178ca86e61986acacfb94fe418bf5f27b4b
azu <azuciao@gmail.com>
2015-11-10 01:54:22 +0900

    chore(related): make link target _blank

--- src/component/RelatedItemList.js
+++ src/component/RelatedItemList.js
@@ -11,7 +11,7 @@ export function render(component) {
         var plainText = removeMarkdown(item.content, {stripListLeaders: false});
         return <div class="RelatedItem">
             <h4><RelatedItem title={item.title} url={item.url}/></h4>
-            <cite><a href={post.url}>{post.title.replace(/(\d{4}-\d{2}-\d{2}のJS).*$/, "$1")}</a></cite>
+            <cite><a href={post.url} title={post.title} target="_blank">{post.title.replace(/(\d{4}-\d{2}-\d{2}のJS).*$/, "$1")}</a></cite>
             <p class="RelatedItem--description">{plainText}</p>
         </div>
 
