157fff7d6e8fee80c9a27f69b0aaac6adb1bc934
azu <azuciao@gmail.com>
2015-11-10 21:27:20 +0900

    Built from dd752a6

--- ko/src/component/RelatedItemList.js
+++ ko/src/component/RelatedItemList.js
@@ -3,16 +3,24 @@
 import element from 'virtual-element'
 import RelatedItem from "./RelatedItem"
 import removeMarkdown from "remove-markdown";
+function ellipsis(text) {
+    if (text.length > 200) {
+        return text.slice(0, 200) + "…";
+    }
+    return text;
+}
 export function render(component) {
     let {props, state, id} = component;
     var items = props.postWithItems.map((postWithItem, index) => {
         let {item, post} = postWithItem;
         // strip markdown
         var plainText = removeMarkdown(item.content, {stripListLeaders: false});
+
         return <div class="RelatedItem">
             <h4><RelatedItem title={item.title} url={item.url}/></h4>
-            <cite><a href={post.url} title={post.title} target="_blank">{post.title.replace(/(\d{4}-\d{2}-\d{2}のJS).*$/, "$1")}</a></cite>
-            <p class="RelatedItem--description">{plainText}</p>
+            <cite><a href={post.url} title={post.title}
+                     target="_blank">{post.title.replace(/(\d{4}-\d{2}-\d{2}のJS).*$/, "$1")}</a></cite>
+            <p class="RelatedItem--description">{ellipsis(plainText)}</p>
         </div>
 
     });
