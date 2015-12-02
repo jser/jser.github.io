f746b6edce32daaa5a88a3348494c73bfdcf24c6
azu <azuciao@gmail.com>
2015-12-02 12:00:04 +0900

    fix(test): fix grep filter

--- test/travis-spellcheck.sh
+++ test/travis-spellcheck.sh
@@ -9,7 +9,7 @@ gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 diffBranchName="origin/develop"
 # 変更行のみを対象にする
 git diff --name-only --diff-filter=ACMR ${diffBranchName} \
-| grep -a '_posts/.*.md$' \
+| grep -a '_i18n/ja/.*.md$' \
 | xargs $(npm bin)/textlint -f checkstyle \
 | checkstyle_filter-git diff ${diffBranchName} \
 | saddler report \
