6faed39888701a187e7f3905973e67ee0c67e8ab
azu <azuciao@gmail.com>
2018-05-03 22:02:48 +0900

    feat(data): add public/data

---
+++ _tools/generate-post-details-json.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+"use strict";
+const glob = require("glob-all");
+const parse = require("@jser/post-parser").parse;
+const fs = require("fs");
+const path = require("path");
+const jaDir = path.join(__dirname, "..", "_i18n/ja");
+const outputFilePath = path.join(__dirname, "..", "public", "data/post-details.json");
+const mdFiles = glob.sync([
+    `${jaDir}/**/*.md`,
+    `!${jaDir}/events/*.md`,
+    `!${jaDir}/support/*.md`,
+    `!${jaDir}/tumblr/*.md`,
+    `!${jaDir}/about/*.md`,
+]);
+/* urlとカテゴリ(記事中の分類)を関連付けたJSONを作る
+    [{
+        category: "ヘッドライン",
+        url: "http://example.com"
+    }]
+ */
+let resultJSON = [];
+mdFiles.forEach(mdFilePath => {
+    const content = fs.readFileSync(mdFilePath, "utf-8");
+    try {
+        const maps = parse(content, {
+            filePath: mdFilePath
+        });
+        resultJSON = resultJSON.concat(maps);
+    } catch (error) {
+        console.log(mdFilePath);
+        console.error(error);
+    }
+});
+
+fs.writeFileSync(outputFilePath, JSON.stringify(resultJSON), "utf-8");
