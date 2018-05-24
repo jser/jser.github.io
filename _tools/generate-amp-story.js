c44e50a823edbcd10d65f80105b3491e1cfb86aa
azu <azuciao@gmail.com>
2018-05-24 19:25:47 +0900

    fix(AMP): update AMP edition

--- _tools/generate-amp-story.js
+++ _tools/generate-amp-story.js
@@ -151,7 +151,11 @@ layout: null
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 </head>
 <body>
-<amp-story standalone bookend-config-src="/ampstory/backend.json">
+<amp-story standalone bookend-config-src="/ampstory/backend.json" 
+title="${meta.title}"
+publisher="JSer.info"
+publisher-logo-src="/public/img/jser-icon.png"
+poster-portrait-src="/public/img/logo-3x4.png">
     ${raw(coverPage.toString())}
     ${raw(pages.map(page => page.toString()).join("\n"))}
 </amp-story>
