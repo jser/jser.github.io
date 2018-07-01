99803f1034d895a4eeed3207fd04fa53960e4299
azu <azuciao@gmail.com>
2018-07-01 13:18:21 +0900

    fix(reviewdog): user .reviewdog.yml

--- test/reviewdog.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git diff --name-only --diff-filter=ACMR origin/develop \
-| grep -a '_i18n/ja/.*.md$' \
-| xargs $(npm bin)/textlint -f checkstyle \
-| reviewdog -f=checkstyle -name="textlint" -reporter=github-pr-check
