4f6b1bdacf65354cc5076698afac2bd89a7858aa
azu <azuciao@gmail.com>
2020-11-21 13:49:22 +0900

    fix

--- actions/update-draft-pr/src/rename.ts
+++ actions/update-draft-pr/src/rename.ts
@@ -18,9 +18,10 @@ interface EmbedHeadlineParams {
 
 const embedHeadline = ({ content, headline }: EmbedHeadlineParams): string => {
     if (!headline) {
+        console.log("headline is not defined");
         return content;
     }
-    return content.replace(/---\s+JSer\.info #(\d+)[\s\S]*\n\n----\n\n<h1/, `---
+    return content.replace(/---\s+JSer\.info #(\d+)[\s\S]+----\n\n<h1/, `---
 
 JSer.info #$1 - ${headline}
 
