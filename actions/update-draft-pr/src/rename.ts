8ab038fa828c9743b452a56d534b4b7df32732ba
azu <azuciao@gmail.com>
2020-11-21 13:22:31 +0900

    title

--- actions/update-draft-pr/src/rename.ts
+++ actions/update-draft-pr/src/rename.ts
@@ -20,11 +20,11 @@ const embedHeadline = ({ content, headline }: EmbedHeadlineParams): string => {
     if (!headline) {
         return content;
     }
-    return content.replace(/---\s+JSer.info #\d+[\s\S]*?----/, `---
+    return content.replace(/---\s+JSer.info #\d+[\s\S]*?----\n\n<h1/, `---
 
 JSer.info #512 - ${headline}
 
-----`);
+----\n\n<h1`);
 }
 /**
  *
