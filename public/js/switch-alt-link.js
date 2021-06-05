c99cb7764b8a81f100b25e29daacd1234c438f95
azu <azuciao@gmail.com>
2021-06-05 13:03:35 +0900

    perf(website): improve performance
    - Remove blocking JS
    - Image Lazy loading
    - Optimize CLS https://web.dev/optimize-cls/#images-without-dimensions

--- public/js/switch-alt-link.js
+++ public/js/switch-alt-link.js
@@ -1,23 +1,33 @@
 // LICENSE : MIT
-// if altHref is not 404, then replace href to altHref
-window.addEventListener("DOMContentLoaded", function () {
-    "use strict";
-    function detectNotFound(URL, callback) {
-        var request = new XMLHttpRequest();
-        request.open("HEAD", URL);
-        request.onload = function () {
-            if (request.status >= 200 && request.status < 300) {
-                callback(null);
-            } else {
-                callback(new Error("status code is " + request.status));
-            }
-        };
-        request.onerror = function () {
-            callback(new Error("status code is " + request.status));
-        };
-        request.send();
-    }
+function onReady() {
+    return new Promise((resolve) => {
+        const readyState = document.readyState;
+        if (readyState === "interactive" || readyState === "complete") {
+            resolve();
+        } else {
+            window.addEventListener("DOMContentLoaded", resolve);
+        }
+    });
+}
 
+function detectNotFound(URL, callback) {
+    var request = new XMLHttpRequest();
+    request.open("HEAD", URL);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 300) {
+            callback(null);
+        } else {
+            callback(new Error("status code is " + request.status));
+        }
+    };
+    request.onerror = function () {
+        callback(new Error("status code is " + request.status));
+    };
+    request.send();
+}
+
+// if altHref is not 404, then replace href to altHref
+function updateAltLink() {
     var altHrefTags = document.querySelectorAll('a[data-alt-href]');
     var links = Array.prototype.slice.call(altHrefTags);
     links.forEach(function (link) {
@@ -30,4 +40,6 @@ window.addEventListener("DOMContentLoaded", function () {
             link.href = altHref;
         });
     });
-});
+}
+
+onReady().then(() => updateAltLink());
