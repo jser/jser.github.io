4cd27f849689cd693d422aea320679b86f5ecafc
azu <azuciao@gmail.com>
2024-01-27 07:42:03 +0900

    chore: remove related-item.js and deps

--- src/component/RelatedItem.js
+++
@@ -1,6 +0,0 @@
-// LICENSE : MIT
-"use strict";
-import {h} from 'preact';
-export default function RelatedItem({url, title}) {
-    return <a className="RelatedItem" href={url} target="_blank">{title}</a>
-}
